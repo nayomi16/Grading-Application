34fec69eec0e175b9af5f2b62b53aa0c077f7496
nayomi16 <nayomirashmikaaa@gmail.com>
2021-02-25 22:15:36 +0530

    updated static data, added node modules

--- controllers/student.js
+++ controllers/student.js
@@ -1,78 +1,81 @@
 const student=require('../staticData/studentData.json')
 const studentResults=require('../staticData/studentResult.json')
 const questions=require('../staticData/question.json')
-
-
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
 
 module.exports.getStudent=(req,res)=>{
-    console.log("heade=",req.headers.authorization)
+    // console.log("heade=",req.headers.authorization)
     if(student.length == 0){
         res.status(403).json({message :"No students available"});  
     }else{
-        let L;
-        console.log(req.params.SId);
-        const attemptAssignments = new Set()
-        let attemptAssignmentsArray = [];
-        student.forEach(s => {
-            if(s.SId==req.params.SId) {
+            let L;
+            console.log(req.params.SId);
+            const attemptAssignments = new Set()
+            let attemptAssignmentsArray = [];
+            student.forEach(s => {
+                if(s.SId==req.params.SId) {
 
-                studentResults.forEach(stuResult => {
-                    if(stuResult.SId==req.params.SId) attemptAssignments.add(stuResult.assId);
+                    studentResults.forEach(stuResult => {
+                        if(stuResult.SId==req.params.SId) attemptAssignments.add(stuResult.assId);
+                    });
+
+                    attemptAssignments.forEach(v => attemptAssignmentsArray.push(v));
+                    var data = { 
+                        "studentObj": s, 
+                        "assignments": attemptAssignmentsArray, 
+                        
+                    };
+
+                    L=req.params.SId;
+                    res.status(200).json({
+                        "code":200,
+                        "massage":"true",
+                        "data":data
+                    }); 
+                }
+                
+            });
+            // console.log(attemptAssignmentsArray);
+            if(L==null){
+                res.status(403).json({message :"Student id is invalid"}); 
+            }
+            }
+    }
+
+module.exports.getDetailResults=(req,res,next)=>{
+    
+            console.log("heade=",req.headers.authorization)
+            const qResults=[];
+            let L;
+            if(studentResults.length == 0){
+                res.status(403).json({message :"No questions available"});  
+            }else{
+                studentResults.forEach(result => {
+                    // console.log(req.params.sId)
+                    if(result.assId==req.params.assId && result.SId==req.params.sId){
+                        
+                    var qResult={
+                        "qId":result.qId,
+                        "resultStatus":result.resultStatus,
+                        "timeSpent":result.timeSpent,
+                        "noOfAttempt":result.noOfAttempt
+                    }
+                    qResults.push(qResult);
+
+                        }
+                        L=req.params.sId;
+                
                 });
-
-                attemptAssignments.forEach(v => attemptAssignmentsArray.push(v));
-                var data = { 
-                    "studentObj": s, 
-                    "assignments": attemptAssignmentsArray, 
-                    
-                  };
-
-                L=req.params.SId;
+                console.log("res=",qResults)
                 res.status(200).json({
                     "code":200,
                     "massage":"true",
-                    "data":data
+                    "data":qResults
                 }); 
-            }
             
-        });
-        console.log(attemptAssignmentsArray);
-        if(L==null){
-            res.status(403).json({message :"Student id is invalid"}); 
-         }
-        }
-}
-
-module.exports.getDetailResults=(req,res)=>{
-    console.log("heade=",req.headers.authorization)
-    const qResults=[];
-    let L;
-    if(studentResults.length == 0){
-        res.status(403).json({message :"No questions available"});  
-    }else{
-        studentResults.forEach(result => {
-            console.log(req.params.sId)
-            if(result.assId==req.params.assId && result.SId==req.params.sId){
-                
-               var qResult={
-                   "qId":result.qId,
-                   "resultStatus":result.resultStatus,
-                   "timeSpent":result.timeSpent,
-                   "noOfAttempt":result.noOfAttempt
-               }
-               qResults.push(qResult);
-
-                 }
-                 L=req.params.sId;
-        
-         });
-         res.status(200).json({
-             "code":200,
-             "massage":"true",
-             "data":qResults
-         }); 
-      
-    }
+            }
+       
 }
 
 module.exports.reviewAnswer=(req,res)=>{
@@ -103,7 +106,7 @@ module.exports.reviewAnswer=(req,res)=>{
                     if(q.qId==result.qId && q.assId==result.assId && q.teachId==teacherId){
                         que=q.question
                         correctAnswer=q.correctAnswer;
-                        console.log(q.question,"--",q.correctAnswe);
+                        console.log(q.question,"--",q.correctAnswer);
                     } 
                 });
                 
--- routes/studentApi.js
+++ routes/studentApi.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const student=require('../controllers/student');
-const token=require('../config/token')
+const jwtHelper=require('../helpers/jwtHelper')
+var jwt = require('jsonwebtoken');
 
-router.get('/:SId', student.getStudent)
+
+router.get('/:SId',student.getStudent)
 router.get('/assignment/:assId/:sId',student.getDetailResults)
 router.get('/assignment/review/:assId/:sId', student.reviewAnswer)
 
